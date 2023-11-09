5625958742635933674d3ac7aae1d3cd252c369c
HBzhainan-wzw <zhiwen_wu@ucsb.edu>
2023-11-09 10:27:12 -0800

    init

---
+++ software/Telemetry/Server.py
@@ -0,0 +1,27 @@
+
+import cv2
+import zmq
+import base64
+import picamera
+
+from picamera.array import PiRGBArray
+
+print("Server Starting")
+IP = '192.168.3.11'
+
+camera = picamera.PiCamera()
+camera.resoulution = (640,480)
+camera.framerate = 20
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+context = zmq.Context()
+footage_socket = context.socket(zmq.PAIR)
+footage_socket.connect('tcp://%s:5555'%IP)
+print("Target IP: " + IP)
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port = True):
+    frame_image = frame.array
+    encoded, buffer = cv2.imencode('.jpg', frame_image)
+    jpg_as_text = base64.b64encode(buffer)
+    footage_socket.send(jpg_as_text)
+    rawCapture.truncate(0)
