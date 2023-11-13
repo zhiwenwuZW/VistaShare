095edada4cce80b6672468572d15f1c9b0249242
HBzhainan-wzw <zhiwen_wu@ucsb.edu>
2023-11-12 18:01:59 -0800

    1

--- software/Telemetry/Client.py
+++ software/Telemetry/Client.py
@@ -7,10 +7,14 @@ context = zmq.Context()
 footage_socket = context.socket(zmq.PAIR)
 footage_socket.bind('tcp://*:5555')
 
-while True:
-    frame = footage_socket.recv_string()
-    img = base64.b64decode(frame)
-    npimg = np.frombuffer(img, dtype=np.uint8)
-    source = cv2.imdecode(npimg, 1)
-    cv2.imshow("Stream", source)
-    cv2.waitKey(1)
+try:
+    while True:
+        frame = footage_socket.recv_string()
+        img = base64.b64decode(frame)
+        npimg = np.frombuffer(img, dtype=np.uint8)
+        source = cv2.imdecode(npimg, 1)
+        cv2.imshow("Stream", source)
+        cv2.waitKey(1)
+except KeyboardInterrupt:
+    print("Interrupted by user")
+    cv2.destroyAllWindows()
--- software/Telemetry/Server.py
+++ software/Telemetry/Server.py
@@ -1,27 +1,29 @@
-
 import cv2
 import zmq
 import base64
 import picamera
-
 from picamera.array import PiRGBArray
 
 print("Server Starting")
 IP = '192.168.10.228'
 
 camera = picamera.PiCamera()
-camera.resoulution = (640,480)
+camera.resolution = (640, 480)
 camera.framerate = 20
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
 context = zmq.Context()
 footage_socket = context.socket(zmq.PAIR)
-footage_socket.connect('tcp://%s:5555'%IP)
-print("Target IP: " + IP)
+footage_socket.connect(f'tcp://{IP}:5555')
+print(f"Target IP: {IP}")
 
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port = True):
-    frame_image = frame.array
-    encoded, buffer = cv2.imencode('.jpg', frame_image)
-    jpg_as_text = base64.b64encode(buffer)
-    footage_socket.send(jpg_as_text)
-    rawCapture.truncate(0)
+try:
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        frame_image = frame.array
+        encoded, buffer = cv2.imencode('.jpg', frame_image)
+        jpg_as_text = base64.b64encode(buffer)
+        footage_socket.send(jpg_as_text)
+        rawCapture.truncate(0)
+except KeyboardInterrupt:
+    print("Interrupted by user")
+    camera.close()
