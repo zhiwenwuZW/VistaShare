6b7ebda4efdac25b4731792b83ee5edb92a9dfd0
HBzhainan-wzw <zhiwen_wu@ucsb.edu>
2023-11-12 18:10:13 -0800

    Update Client.py

--- software/Telemetry/Client.py
+++ software/Telemetry/Client.py
@@ -7,14 +7,15 @@ context = zmq.Context()
 footage_socket = context.socket(zmq.PAIR)
 footage_socket.bind('tcp://*:5555')
 
-try:
-    while True:
-        frame = footage_socket.recv_string()
-        img = base64.b64decode(frame)
-        npimg = np.frombuffer(img, dtype=np.uint8)
-        source = cv2.imdecode(npimg, 1)
-        cv2.imshow("Stream", source)
-        cv2.waitKey(1)
-except KeyboardInterrupt:
-    print("Interrupted by user")
-    cv2.destroyAllWindows()
+while True:
+    frame = footage_socket.recv_string()
+    img = base64.b64decode(frame)
+    npimg = np.frombuffer(img, dtype=np.uint8)
+    source = cv2.imdecode(npimg, 1)
+    cv2.imshow("Stream", source)
+
+    # Check if 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
