c0de9043cc025cd02256dd97bd6d7a9a52802325
HBzhainan-wzw <zhiwen_wu@ucsb.edu>
2023-11-13 16:52:24 -0800

    Update Client.py

--- software/Telemetry/Client.py
+++ software/Telemetry/Client.py
@@ -3,20 +3,25 @@ import zmq
 import base64
 import numpy as np
 
-context = zmq.Context()
-footage_socket = context.socket(zmq.PAIR)
-footage_socket.bind('tcp://*:5556')
+class VideoClient:
+    def __init__(self):
+        # Initialize ZMQ socket and other necessary variables
+        self.context = zmq.Context()
+        self.footage_socket = self.context.socket(zmq.PAIR)
+        self.footage_socket.bind('tcp://*:5555')  # Update the address as needed
 
-try:
-    while True:
-        print("1")
-        frame = footage_socket.recv_string()
-        img = base64.b64decode(frame)
-        npimg = np.frombuffer(img, dtype=np.uint8)
-        source = cv2.imdecode(npimg, 1)
-        cv2.imshow("Stream", source)
-        print("2")
-        cv2.waitKey(1)
-except KeyboardInterrupt:
-    print("Interrupted")
-    cv2.destroyAllWindows()
+    def receive_video(self):
+        while True:
+            frame = self.footage_socket.recv_string()
+            img = base64.b64decode(frame)
+            npimg = np.frombuffer(img, dtype=np.uint8)
+            source = cv2.imdecode(npimg, 1)
+            cv2.imshow("Stream", source)
+            # Process the frame (e.g., display, or pass to another function)
+            cv2.waitKey(1)
+
+
+# # Example usage
+# if __name__ == "__main__":
+#     client = VideoClient()
+#     client.receive_video()
