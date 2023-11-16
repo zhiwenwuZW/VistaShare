d0491d14907375691763840b1fbdc08d1f0fdc19
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2023-11-16 11:31:14 -0800

    updated telemetry

--- software/Telemetry/Client.py
+++ software/Telemetry/Client.py
@@ -1,37 +1,41 @@
 import cv2
-import zmq
-import base64
+import socket
 import numpy as np
-import sys
 
-class VideoClient:
-    def __init__(self):
-        # Initialize ZMQ socket and other necessary variables
-        self.context = zmq.Context()
-        self.footage_socket = self.context.socket(zmq.PAIR)
-        self.footage_socket.bind('tcp://*:5555')  # Update the address as needed
-        print("Client Initialized")
+# Server's IP and PORT
+SERVER_IP = '192.168.10.228'
+SERVER_PORT = 5555
 
-    def receive_video(self):
-        global image_buffer
-        print("Client Start Receive Video")
-        while True:
-            frame = self.footage_socket.recv_string()
-            img = base64.b64decode(frame)
-            npimg = np.frombuffer(img, dtype=np.uint8)
-            source = cv2.imdecode(npimg, 1)
+# Create a UDP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.bind((SERVER_IP, SERVER_PORT))
 
-            cv2.imwrite('image.jpg', source)
-            # cv2.imshow("Stream", source)
-            print(".")
-            cv2.waitKey(1)
+print("Client Start Receive Video")
 
+try:
+    while True:
+        # Receive data
+        packet, _ = client_socket.recvfrom(65536)  # Adjust buffer size as needed
 
-# # Example usage
-if __name__ == "__main__":
-    client = VideoClient()
-    try:
-        client.receive_video()
-    except KeyboardInterrupt:
-        print("Interrputed")
-        sys.exit()
+        # Convert packet data to image
+        npimg = np.frombuffer(packet, dtype=np.uint8)
+        source = cv2.imdecode(npimg, 1)
+
+        # Display the image
+        cv2.imshow("Stream", source)
+
+        # Save the current frame as image.jpg
+        cv2.imwrite('image.jpg', source)
+
+        # Break loop with a key press
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+except KeyboardInterrupt:
+    print("Interrupted by user")
+
+finally:
+    # Close the socket and release resources
+    client_socket.close()
+    cv2.destroyAllWindows()
+    print("Client resources have been freed.")
--- software/Telemetry/Server.py
+++ software/Telemetry/Server.py
@@ -1,37 +1,35 @@
 import cv2
-import zmq
-import base64
-import picamera
+import socket
+from picamera import PiCamera
 from picamera.array import PiRGBArray
 
 print("Server Starting")
-IP = '192.168.10.228'
+IP = '192.168.10.228'  # IP of the receiver
+PORT = 5555  # Port of the receiver
 
-camera = picamera.PiCamera()
+camera = PiCamera()
 camera.resolution = (480, 480)
 camera.framerate = 1
 rawCapture = PiRGBArray(camera, size=(480, 480))
 
-context = zmq.Context()
-footage_socket = context.socket(zmq.PAIR)
-footage_socket.connect(f'tcp://{IP}:5555')
-print(f"Target IP: {IP}")
+# Create a UDP socket
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         print(".")
         frame_image = frame.array
         encoded, buffer = cv2.imencode('.jpg', frame_image)
-        jpg_as_text = base64.b64encode(buffer)
-        footage_socket.send(jpg_as_text)
+
+        # Send the image over UDP
+        udp_socket.sendto(buffer, (IP, PORT))
+
         rawCapture.truncate(0)
 
 except KeyboardInterrupt:
     print("Interrupted by user")
     # Close the camera
     camera.close()
-    # Close the ZMQ socket
-    footage_socket.close()
-    # Terminate the ZMQ context
-    context.term()
-    print("Camera and ZMQ resources have been freed.")
+    # Close the UDP socket
+    udp_socket.close()
+    print("Camera and UDP socket resources have been freed.")
