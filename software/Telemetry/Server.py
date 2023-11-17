6807f092633e694ea0ae9b0f6eff5f0108282455
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2023-11-17 15:15:59 -0800

    debug

--- software/Telemetry/Server.py
+++ software/Telemetry/Server.py
@@ -1,37 +1,55 @@
-import cv2
-import zmq
-import base64
+# import cv2
+# import zmq
+# import base64
+# import picamera
+# from picamera.array import PiRGBArray
+
+# print("Server Starting")
+# IP = '192.168.10.228'
+
+# camera = picamera.PiCamera()
+# camera.resolution = (640, 480)
+# camera.framerate = 10
+# rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# context = zmq.Context()
+# footage_socket = context.socket(zmq.PAIR)
+# footage_socket.connect(f'tcp://{IP}:5556')
+# print(f"Target IP: {IP}")
+
+# try:
+#     cnt = 0
+#     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#         if cnt > 10:
+#             frame_image = frame.array
+#             encoded, buffer = cv2.imencode('.jpg', frame_image)
+#             jpg_as_text = base64.b64encode(buffer)
+#             footage_socket.send(jpg_as_text)
+#             cnt = 0
+#         else: 
+#             cnt = cnt + 1
+#             rawCapture.truncate(0)
+# except KeyboardInterrupt:
+#     print("Interrupted by user")
+#     footage_socket.close()
+#     context.term()
+#     camera.close()
+#     print("Resource Freed")
+
+
+
 import picamera
 from picamera.array import PiRGBArray
-
-print("Server Starting")
-IP = '192.168.10.228'
 
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 10
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
-context = zmq.Context()
-footage_socket = context.socket(zmq.PAIR)
-footage_socket.connect(f'tcp://{IP}:5556')
-print(f"Target IP: {IP}")
-
 try:
-    cnt = 0
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        if cnt > 10:
-            frame_image = frame.array
-            encoded, buffer = cv2.imencode('.jpg', frame_image)
-            jpg_as_text = base64.b64encode(buffer)
-            footage_socket.send(jpg_as_text)
-            cnt = 0
-        else: 
-            cnt = cnt + 1
-            rawCapture.truncate(0)
+        print("Frame captured")
+        rawCapture.truncate(0)
 except KeyboardInterrupt:
     print("Interrupted by user")
-    footage_socket.close()
-    context.term()
     camera.close()
-    print("Resource Freed")
