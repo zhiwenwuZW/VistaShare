c2f1ade82d911b5cb42b097a2dbe420587b043c9
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2023-11-29 13:43:12 -0800

    updated model

---
+++ software/Telemetry/DetectModel.py
@@ -0,0 +1,76 @@
+from ultralytics import RTDETR
+import cv2
+import numpy as np
+import argparse
+import time
+import ast
+import torch
+from PIL import Image
+import sys
+import os
+import threading
+import zmq
+import base64
+
+
+
+
+
+class Detector:
+    def __init__(self):
+        # Initialize ZMQ context and socket
+        self.context = zmq.Context()
+        self.footage_socket = self.context.socket(zmq.PAIR)
+        self.footage_socket.bind('tcp://*:5556')
+        print("Client Init")
+
+
+    def receive(self):
+        frame = self.footage_socket.recv_string()
+        img = base64.b64decode(frame)
+        npimg = np.frombuffer(img, dtype=np.uint8)
+        source = cv2.imdecode(npimg, 1)
+        print("Received")
+        cv2.imshow("Stream", source)
+
+        source_rgb = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
+        self.input = Image.fromarray(source_rgb)
+
+    def detect(self):
+        # Load the model
+        model = RTDETR('rtdetr-l.pt')
+
+        # Run inference
+        # only keep 4 objects: 2: cars  5: bus 7: truck 9: traffic light 10:fire hydrant 11: stop sign 
+        results = model('self.input', stream=True, classes = [2, 5, 7, 9, 10, 11])
+
+        for result in results:
+            # print(result.boxes.xyxy)
+            classes = result.boxes.cls
+            # Calculate the center of the image
+            img_center_x = result.orig_shape[1] / 2
+            img_center_y = result.orig_shape[0] *2 / 3
+
+            print("pic center is:" + str(img_center_x) + " " + str(img_center_y))
+
+            for box in result.boxes.xyxy:
+                x1, y1, x2, y2 = box
+                if x1 <= img_center_x <= x2 and y1 <= img_center_y <= y2:
+                    print("Box containing the center:", box)
+
+
+            print("### Visualize")
+            # Visualize the detections
+            annotated_image = result.plot(masks=False, prob=False, conf=False, labels=True)
+            cv2.imshow('Detected Objects', annotated_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__":
+    detector = Detector()
+    print("Start Detect")
+    while True:
+        detector.receive()
+        detector.detect()
