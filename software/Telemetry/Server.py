b130680d2889950c846795a8630536edce538894
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2023-12-06 15:24:18 -0800

    debug

--- software/Telemetry/Server.py
+++ software/Telemetry/Server.py
@@ -27,29 +27,29 @@ def wait_until(target_time):
 start_time = datetime.datetime.now() + datetime.timedelta(seconds=5)
 print(f"Start time set for {start_time}")
 
-try:
-    print("Waiting for synchronized start time...")
-    wait_until(start_time)  # Wait until the start time
+# try:
+print("Waiting for synchronized start time...")
+wait_until(start_time)  # Wait until the start time
 
-    print("Starting camera capture...")
-    while True:
-        ret, frame = camera.read()
-        if not ret:
-            break  # Exit the loop if the frame is not captured successfully
+print("Starting camera capture...")
+while True:
+    ret, frame = camera.read()
+    if not ret:
+        break  # Exit the loop if the frame is not captured successfully
 
-        # Process the frame here (e.g., display, save, or send it)
-        # Increase JPEG compression
-        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 70]  # Example: JPEG quality set to 70
-        result, buffer = cv2.imencode('.jpg', frame, encode_param)
+    # Process the frame here (e.g., display, save, or send it)
+    # Increase JPEG compression
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 70]  # Example: JPEG quality set to 70
+    result, buffer = cv2.imencode('.jpg', frame, encode_param)
 
-        # Encode the frame for transmission
-        _, buffer = cv2.imencode('.jpg', frame)
-        sock.sendto(buffer.tobytes(), (IP, PORT))
+    # Encode the frame for transmission
+    _, buffer = cv2.imencode('.jpg', frame)
+    sock.sendto(buffer.tobytes(), (IP, PORT))
 
-except KeyboardInterrupt:
-    print("Interrupted by user")
+# except KeyboardInterrupt:
+#     print("Interrupted by user")
 
-finally:
-    camera.release()
-    sock.close()
-    print("Camera closed. Resource Freed")
+# finally:
+#     camera.release()
+#     sock.close()
+#     print("Camera closed. Resource Freed")
