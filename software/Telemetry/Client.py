c36cfdfe63f428b71161c5f4dbea27900ca95d32
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2023-12-05 17:36:57 -0800

    updated UDP ntp streaming

--- software/Telemetry/Client.py
+++ software/Telemetry/Client.py
@@ -1,36 +1,44 @@
 import cv2
 import socket
 import numpy as np
+import threading
 
-# Set the IP address and port to match the server
-IP = '0.0.0.0'  # Use '0.0.0.0' to bind to all available interfaces
-PORT = 12345  # The port should match the one used by the server
+# Function to handle each stream
+def receive_stream(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', port))
 
-# Initialize a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((IP, PORT))
+    cv2.namedWindow(str(port))  # Create a window for each stream
 
-try:
-    print("Starting video stream reception...")
-    while True:
-        # Receive data from the UDP socket
-        packet, _ = sock.recvfrom(65535)  # Buffer size; adjust as needed
+    try:
+        while True:
+            packet, _ = sock.recvfrom(65535)  # Adjust buffer size as needed
+            nparr = np.frombuffer(packet, np.uint8)
+            frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-        # Reconstruct the frame
-        nparr = np.frombuffer(packet, np.uint8)
-        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            if frame is not None:
+                cv2.imshow(str(port), frame)
 
-        # Display the frame
-        cv2.imshow('Video Stream', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        # Exit loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    finally:
+        sock.close()
+        cv2.destroyWindow(str(port))
 
-except KeyboardInterrupt:
-    print("Stream reception interrupted by user")
+# Ports for the two Raspberry Pi streams
+ports = [12345, 12346]
 
-finally:
-    sock.close()
-    cv2.destroyAllWindows()
-    print("Socket closed and OpenCV resources released")
+# Create and start threads for each stream
+threads = []
+for port in ports:
+    thread = threading.Thread(target=receive_stream, args=(port,))
+    thread.start()
+    threads.append(thread)
+
+# Wait for all threads to complete
+for thread in threads:
+    thread.join()
+
+cv2.destroyAllWindows()
+print("All streams closed.")
