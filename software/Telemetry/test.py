058dbd31bdfc1c0154e7cdc29db4737c33801015
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2023-12-06 15:34:51 -0800

    debug

---
+++ software/Telemetry/test.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
+if not cap.isOpened():
+    print("Camera can't be opened")
+else:
+    ret, frame = cap.read()
+    if ret:
+        print("Frame captured")
+        # Optionally display the frame using cv2.imshow()
+    else:
+        print("Failed to capture frame")
+
+cap.release()
