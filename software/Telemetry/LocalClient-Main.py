5fe38a3acc31d16be73836b04a0868139133e00a
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2023-12-07 19:33:54 -0800

    optimize

--- software/Telemetry/LocalClient-Main.py
+++ software/Telemetry/LocalClient-Main.py
@@ -11,8 +11,8 @@ port = 8888
 model = YOLO('yolov8n.pt')
 tcp_addr = f'tcp://{ip_addr}:{port}'
 # using local ip_addr
-# only keep objects: 2: cars  5: bus 7: truck 9: traffic light 10:fire hydrant 11: stop sign 
-results = model(tcp_addr, stream=True, classes = [0, 2, 5, 7, 9, 10, 11])
+# only keep objects: 0: human 2: cars  5: bus 7: truck 9: traffic light 10:fire hydrant 11: stop sign 
+results = model(tcp_addr, stream=False, classes = [2, 5, 7, 9, 10, 11])
 
 # Set up TCP socket
 tcp_ip = "192.168.52.228"
