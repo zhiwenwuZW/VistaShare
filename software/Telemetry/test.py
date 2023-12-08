7e6c846cd68f8dfcecac47d07012c8aafa796f2c
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2023-12-07 19:36:30 -0800

    optimize

--- software/Telemetry/test.py
+++ software/Telemetry/test.py
@@ -2,9 +2,3 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
 results = model('tcp://127.0.0.1:8888', stream=True)
-
-while True:
-    for result in results:
-        boxes = result.boxes
-        probs = result.probs
-        print(boxes)
